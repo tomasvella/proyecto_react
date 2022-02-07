cd3d164699ef3cdabfc63d67df61f0dba887ccb2
Tomas Vella <vella.tomas@gmail.com>
2022-02-07 17:11:43 -0300

    actualización de stock en venta y estilos finales

--- src/db/firebase.js
+++ src/db/firebase.js
@@ -1,10 +1,6 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: 'AIzaSyB3Ie1byvmkON0dnE_tPX3JHGyRUEwmb5E',
   authDomain: 'proyecto-react-a7742.firebaseapp.com',
@@ -14,8 +10,6 @@ const firebaseConfig = {
   appId: '1:303362226289:web:0035ae8eecc509811b03d0',
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-// Initialize Firestore
 export const db = getFirestore(app);
--- src/db/queryDetail.js
+++ src/db/queryDetail.js
@@ -2,17 +2,13 @@ import { db } from './firebase';
 import { doc, getDoc, collection } from 'firebase/firestore';
 
 export const queryDetail = (id, setProduct, setLoading) => {
-  // Acceso a la colección
   const collectionDb = collection(db, 'productos');
 
-  // Acceso a un documento
   const consult = doc(collectionDb, id);
   getDoc(consult)
     .then((result) => {
-      // Se obtienen los datos del documento
       const id = result.id;
       const data = result.data();
-      // Se crea un objeto nuevo con el id y los datos del documento
       const product = { id, ...data };
       setProduct(product);
       setLoading(false);
